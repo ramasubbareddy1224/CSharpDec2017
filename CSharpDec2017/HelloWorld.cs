0490bbcf4e3e0e208df60da2fe1603607c34e9f9
ramasubba.m <ramasubba.m@cgi.com>
2017-12-12 08:53:51 +0530

    dec12

---
+++ CSharpDec2017/HelloWorld.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpDec2017.Learning
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World Progarm");
+            Console.WriteLine("Second Line");
+
+            short scount = short.MaxValue;
+
+            Console.WriteLine("short maxvalue:"+scount);
+            
+            int count = 10;
+
+            int temparature = -20;
+            
+            Console.WriteLine("before count:" + count);
+            string name = "pragim tech";
+
+            count = 20;
+            count = 40;
+            Console.WriteLine("after count:" + count);
+
+            char gender = 'M';
+
+            double percent = 73.8;
+
+            bool foodAvailable = true;
+
+            int temp;
+           // Console.WriteLine("print temp value:"+temp);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpDec2017/TypeConversion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDec2017
+{
+    class TypeConversion
+    {
+        static void Main()
+        {
+            // Boxing
+
+            int i = 10;
+            string converti = i.ToString();  // way 1
+            string converti2 = Convert.ToString(i);  // way 2
+
+            Console.WriteLine("printing i value:" + i);
+            Console.WriteLine("printing converti value:" + converti);
+
+
+            // UnBoxing
+            string sval = "100";
+            int unboxint =Convert.ToInt32(sval);
+            double d = Convert.ToDouble(sval);
+
+
+
+            object o = i;
+            object o1 = converti;
+            object o2 = 43.5;
+            object o3 = "100aba basdd fs";
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
