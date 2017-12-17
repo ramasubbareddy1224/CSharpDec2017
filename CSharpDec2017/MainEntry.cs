a59f7c824bb4d7180a64ac47bf40fe287cbd696a
ramasubba.m <ramasubba.m@cgi.com>
2017-12-17 09:30:08 +0530

    dec17

---
+++ CSharpDec2017/MainEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDec2017
+{
+    public class MainEntry
+    {
+        static void Main()
+        {
+            Student studentObj = new Student();
+            studentObj.GetStudent();
+            studentObj.GetAllStudents();
+
+            studentObj.id = 10;
+
+            int studentId = studentObj.getStudentId();
+
+            Console.WriteLine("student id:" + studentId);
+
+            studentObj.name = "rama";
+            string stringname = studentObj.getStudentName();
+
+            Console.WriteLine("student name:" + stringname);
+
+            Student studentObj1 = new Student();
+            studentObj1.name = "pragim";
+            string sname = studentObj1.getStudentName();
+            Console.WriteLine("student1 obj :name:" + sname);
+
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpDec2017/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDec2017
+{
+    class Student
+    {
+        public int id;
+        public string name;
+        public string email;
+
+        public int studentid = 100;
+        public void GetStudent()
+        {
+            Console.WriteLine("get student method");
+        }
+        public void GetAllStudents()
+        {
+            Console.WriteLine("Get all student method");
+        }
+        public int getStudentId()
+        {
+            //int a = 10;
+            //int b = a + 20;
+            Console.WriteLine("inside getstudetid");
+            return id;
+            
+
+        }
+        public string getStudentName()
+        {
+            return name;
+        }
+    }
+}
